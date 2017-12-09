bcacea7268803f8f4487200b11866e3a458ffc79
Gayan Lakshitha Gunarathna <gliger.glg@gmail.com>
2017-12-09 22:43:29 +0530

    New Update of Page

---
+++ js/btn-hover.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#btn_round').hover(function(){
+		$(this).css("background-color","cyan");
+	},function(){
+		$(this).css("background-color","transparent");
+	});
+});
